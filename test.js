5c7e4b880a672fb5b715922c5467f8f6a041be3d
camlump <camlump@yahoo.com>
2022-07-22 00:35:37 -0400

    commit five

--- test.js
+++ test.js
@@ -1,2 +1,17 @@
 console.log("hello world");
 
+const multiply = ( a, b) => {
+    return a * b;
+}
+
+const drinkCheck = ( age ) => {
+    if(age < 21 ) {
+        return "yo are not allowed to drink because you are " + age;
+    } else if( age >= 21) {
+     return "yo are allowed to drink because you are over the " + age;
+    } else{
+        return" I don't know what this means";
+    }
+}
+
+console.log(drinkCheck(18))
---
+++ test2.js
@@ -0,0 +1,18 @@
+
+const add = (a, b) => {
+    return a + b;
+};
+
+
+const subtract = ( a, b) => {
+    return a - b;
+}
+
+const multiply = ( a, b) => {
+    return a * b;
+}
+
+
+console.log(add(1,3));
+console.log(subtract(2,2));
+console.log(multiply(2,2));
